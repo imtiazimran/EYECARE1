de9b8bf1af9c65f188d92d47cc2fa151329f4373
afzal-2004 <moa44468@gmail.com>
2024-01-05 17:15:20 +0530

    modified Error Page

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,10 +1,14 @@
 // ErrorPage.jsx
-
 const ErrorPage = () => {
+  const goBack = () => {
+    history.goBack(); // This will go back to the previous page in the history
+  };
+
   return (
     <div className=" text-center text-[25px] mt-[15%]">
       <h1>404 Not Found</h1>
       <p>{`Oops! The page you're looking for not founnd 🥺🥺🥺🥺 `}</p>
+      <button onClick={goBack}>Go Back</button>
     </div>
   );
 };
