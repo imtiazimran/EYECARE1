80d58e296e94a2d0860cce440eda682c84f6f694
afzal-2004 <moa44468@gmail.com>
2024-01-05 16:32:52 +0530

    modified

--- src/Components/Store/BigScreenStore.jsx
+++ src/Components/Store/BigScreenStore.jsx
@@ -1,11 +1,13 @@
 // import { useState } from "react";
 
+import { Outlet } from "react-router-dom";
 import HOME from "../../Pages/Home";
 import { Footer } from "../footer";
 export const BIGSCREENSTORE = () => {
   return (
     <>
       <HOME></HOME>
+      <Outlet></Outlet>
       <Footer></Footer>
     </>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,37 @@
 /* eslint-disable react-refresh/only-export-components */
 // import { BELOWNAVIGATION } from "../Components/shopnv";
 import BELOWNAVIGATION from "../Components/shopnv";
-
-import { HOMESTR } from "../Components/Store/Homestore";
-
+import { HEROIMAGE } from "../Components/NextHeader";
+//  import  ISLIDER  from""
+import { SRK } from "../Components/card";
+// import { HOMESTR } from "../Components/Store/Homestore";
+import Card from "../Components/card";
+import { Middle } from "../Components/Cards";
+import { SCOCIALMEDIAINFLUNCER } from "../Components/card";
+import { TRENDING } from "../Components/card";
+import { POPULAR } from "../Components/card";
+import { MOBILEPOPULAR } from "../Components/card";
+import { MobileStyleMe } from "../Components/card";
+import { FooterImage } from "../Components/Cards";
+import ISLIDER from "../Components/Islider";
 const HOME = () => {
   return (
     <>
       <div className="z-0">
         <div>
           <BELOWNAVIGATION></BELOWNAVIGATION>
-          <HOMESTR></HOMESTR>
+          <HEROIMAGE></HEROIMAGE>
+          <ISLIDER></ISLIDER>
+          <Card></Card>
+          <Middle></Middle>
+          <SRK></SRK>
+          <MobileStyleMe></MobileStyleMe>
+          <MOBILEPOPULAR></MOBILEPOPULAR>
+          <SCOCIALMEDIAINFLUNCER></SCOCIALMEDIAINFLUNCER>
+          <TRENDING></TRENDING>
+          <POPULAR></POPULAR>
+          <FooterImage></FooterImage>
+          {/* <HOMESTR></HOMESTR> */}
         </div>
       </div>
     </>
