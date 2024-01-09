0c35845c83144ba9385bab6f45260432f8bbe424
afzal-2004 <moa44468@gmail.com>
2024-01-09 05:33:47 +0530

    modified

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,28 +4,28 @@ import Navbar from "../../Components/Navbar";
 import ShopNavigation from "../../Components/shopnv";
 
 // import FIRST from "../../Components/images/Aboutus/First.png";
-import FIRST from "./Aboutus/First.png";
+import FIRST from "./Images/First.png";
 
 // import SECOND from "../../Components/images/Aboutus/Second.png";
-import SECOND from "./Aboutus/Second.png";
+import SECOND from "./Images/Second.png";
 
 // import THIRD from "../../Components/images/Aboutus/Third.png";
-import THIRD from "./Aboutus/Third.png";
+import THIRD from "./Images/Third.png";
 
 // import FORTH from "../../Components/images/Aboutus/Forth.png";
-import FORTH from "./Aboutus/Forth.png";
+import FORTH from "./Images/Forth.png";
 
 // import video from "../../Components/images/Aboutus/five.png";
-import video from "./Aboutus/five.png";
+import video from "./Images/five.png";
 
 // import FIVE from "../../Components/images/Aboutus/Rectangle 2959.png";
-import FIVE from "./Aboutus/Rectangle 2959.png";
+import FIVE from "./Images/Rectangle 2959.png";
 
 // import SIX from "../../Components/images/Aboutus/Rectangle 2960.png";
-import SIX from "./Aboutus/Rectangle 2960.png";
+import SIX from "./Images/Rectangle 2960.png";
 
 // import SEVEN from "../../Components/images/Aboutus/Rectangle 2962.png";
-import SEVEN from "./Aboutus/Rectangle 2962.png";
+import SEVEN from "./Images/Rectangle 2962.png";
 
 // import { TeamMember } from "../../Components/card";
 import { TeamMember } from "../../Components/card";
